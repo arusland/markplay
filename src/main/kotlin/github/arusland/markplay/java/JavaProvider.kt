52abc15e018deb96f45e8637be4444545b8de7a6
Ruslan A <ruslan.absalyamov@gmail.com>
2023-03-26 20:35:13 +0200

    Fix removing temp dir and user-friendly error output

--- src/main/kotlin/github/arusland/markplay/java/JavaProvider.kt
+++ src/main/kotlin/github/arusland/markplay/java/JavaProvider.kt
@@ -19,44 +19,46 @@ class JavaProvider : LanguageProvider {
 
     override fun execute(code: String, output: OutputStream): Any? {
         val tempDir = createTempDir()
-        // write code to temp file
-        val className = extractClassName(code)
-        val javaFile = tempDir.resolve("$className.java")
-        Files.write(javaFile, code.toByteArray())
-        val javaBinDir = resoleJavaBin()
-        val javacPath = javaBinDir.resolve("javac").toString()
-        // compile temp file
-        log.debug("Writing code to temp file and compile: {}", javaFile)
-        val compileCmd = listOf(javacPath, javaFile.toString())
-        val compileStdOutput = StringWriter()
-        val compileErrorOutput = StringWriter()
-        val complileExitCode = ExecUtil.runCommand(compileCmd, compileStdOutput, compileErrorOutput)
-        if (complileExitCode != 0) {
-            // TODO: handle error output in different way
-            output.writeFrom(compileStdOutput)
-            compileStdOutput.write("=====compilation failed with code: $complileExitCode =====")
-            compileStdOutput.write(NEWLINE)
-            output.writeFrom(compileErrorOutput)
+        try {
+            // write code to temp file
+            val className = extractClassName(code)
+            val javaFile = tempDir.resolve("$className.java")
+            Files.write(javaFile, code.toByteArray())
+            val javaBinDir = resoleJavaBin()
+            val javacPath = javaBinDir.resolve("javac").toString()
+            // compile temp file
+            log.debug("Writing code to temp file and compile: {}", javaFile)
+            val compileCmd = listOf(javacPath, javaFile.toString())
+            val compileStdOutput = StringWriter()
+            val compileErrorOutput = StringWriter()
+            val complileExitCode = ExecUtil.runCommand(compileCmd, compileStdOutput, compileErrorOutput)
+            if (complileExitCode != 0) {
+                // TODO: handle error output in different way
+                output.writeFrom(compileStdOutput)
+                compileStdOutput.write("=====compilation failed with code: $complileExitCode =====")
+                compileStdOutput.write(NEWLINE)
+                output.write(removeTempPath(compileErrorOutput, tempDir))
+
+                return null
+            }
+            log.debug("Running compiled class: {} in {}", className, tempDir)
+            // run compiled class
+            val javaPath = javaBinDir.resolve("java").toString()
+            val javaClassPath = tempDir.toString()
+            val runCmd = listOf(javaPath, "-cp", javaClassPath, className)
+            val runOutput = StringWriter()
+            val runErrorOutput = StringWriter()
+            val runExitCode = ExecUtil.runCommand(runCmd, runOutput, runErrorOutput)
+            output.writeFrom(runOutput)
+            if (runExitCode != 0) {
+                output.writeFrom(runErrorOutput)
+            }
 
             return null
+        } finally {
+            log.debug("Deleting temp directory: {}", tempDir)
+            tempDir.toFile().deleteRecursively()
         }
-        log.debug("Running compiled class: {} in {}", className, tempDir)
-        // run compiled class
-        val javaPath = javaBinDir.resolve("java").toString()
-        val javaClassPath = tempDir.toString()
-        val runCmd = listOf(javaPath, "-cp", javaClassPath, className)
-        val runOutput = StringWriter()
-        val runErrorOutput = StringWriter()
-        val runExitCode = ExecUtil.runCommand(runCmd, runOutput, runErrorOutput)
-        output.writeFrom(runOutput)
-        if (runExitCode != 0) {
-            output.writeFrom(runErrorOutput)
-        }
-
-        log.debug("Deleting temp directory: {}", tempDir)
-        tempDir.toFile().deleteRecursively()
-
-        return null
     }
 
     /**
@@ -85,6 +87,17 @@ class JavaProvider : LanguageProvider {
         }
 
         return javaBin.toAbsolutePath()
+    }
+
+    /**
+     * Remove temp path from error output
+     */
+    private fun removeTempPath(compileErrorOutput: StringWriter, tempDir: Path): ByteArray {
+        val tempDirStr = tempDir.toString().let { if (it.endsWith("/")) it else "$it/" }
+        val errorOutput = compileErrorOutput.toString()
+        val result = errorOutput.replace(tempDirStr, "")
+
+        return result.toByteArray()
     }
 
     private companion object {
